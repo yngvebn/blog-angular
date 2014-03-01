895fd5dcc02c3d291e81bbaef119fc3560721e37
Yngve Bakken Nilsen <yngve.nilsen@gmail.com>
2014-03-01 11:59:33 +0100

    Part 2 done

---
+++ src/Web/app/Home/Home.js
@@ -0,0 +1,6 @@
+﻿angular.module('app').controller('Home',['$scope', function($scope) {
+        $scope.showTextValue = function() {
+            alert('The value of $scope.text is: ' + $scope.text);
+        };
+    }]
+);
