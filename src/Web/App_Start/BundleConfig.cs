9377223136b48abc1a9360d507d45967fffb0748
Yngve Bakken Nilsen <yngve.nilsen@gmail.com>
2014-03-01 11:06:33 +0100

    Initial commit

---
+++ src/Web/App_Start/BundleConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Web
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/js/lib").Include("~/Scripts/angular.js").IncludeDirectory("~/Scripts/", "*.js"));
+
+            bundles.Add(new ScriptBundle("~/js/app").Include("~/js/app/app.js").IncludeDirectory("~/app", "*.js", true));
+        }
+    }
+}
