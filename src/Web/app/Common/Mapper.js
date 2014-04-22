f022274890e4abe9fee6c0c53b49c47ca77c9b74
Yngve Bakken Nilsen <yngve.nilsen@gmail.com>
2014-04-22 18:54:08 +0200

    Stuff

---
+++ src/Web/app/Common/Mapper.js
@@ -0,0 +1,32 @@
+﻿angular.module('app').service('Mapper', [
+    function() {
+        var transformObject = function (jsonResult, constructor, mapping) {
+            var model = new constructor(), dest = {};
+
+            angular.extend(dest, jsonResult);
+            for (var key in dest) {
+                if (!model.hasOwnProperty(key)) {
+                    delete dest[key];
+                }
+            }
+            angular.forEach(mapping, function(mapFunc, k) {
+                dest[k] = mapFunc.bind(jsonResult)();
+            });
+            angular.extend(model, dest);
+            return model;
+        };
+
+        this.map = function (jsonResult, constructor, mapping) {
+            if (angular.isArray(jsonResult)) {
+                var models = [];
+                angular.forEach(jsonResult, function (object) {
+                    models.push(transformObject(object, constructor, mapping || {}));
+                });
+                return models;
+            } else {
+                return transformObject(jsonResult, constructor, mapping || {});
+            }
+        };
+
+   }
+]);
